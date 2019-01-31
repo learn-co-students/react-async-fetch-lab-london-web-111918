884d80aa9fcc90c0902bc5f11977baf9c1c5349b
IMH51 <ianmartinharrison@outlook.com>
2019-01-31 11:53:26 +0000

    added fetch to react app using componentdidmount lifecycle function

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { Component} from 'react'
+
+export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      astronauts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+    .then(response => response.json())
+    .then(data => this.setState({astronauts: data.people}))
+  }
+
+  render() {
+    return <div>{this.state.astronauts.map(astro => `${astro.name} is currently aboard ${astro.craft}`)}</div>
+  }
+
+}
