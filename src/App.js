176e3e887894e64ff986077be4425e4b1bec95b0
Metahaem <tom.rowson@gmail.com>
2019-02-04 18:23:13 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React from "react";
+
+class App extends React.Component {
+
+    state = {
+        peopleInSpace: []
+    }
+
+    render() {
+        return (
+          <div>
+            {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+          </div>
+        )
+      }
+      
+    componentDidMount () {
+        
+        const URL = `http://api.open-notify.org/astros.json`;
+        return fetch(URL)
+        .then(res => res.json())
+        .then(({people}) => this.setState({ peopleInSpace: people }))
+    }
+
+}
+
+export default App;
